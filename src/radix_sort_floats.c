c81c6c56ae3c5bb33f3ba867efc910ac8b4af0a5
jackr276 <jack_m_robbins@me.com>
2024-03-22 19:49:11 -0400

    runner script made, initial work begun

---
+++ src/radix_sort_floats.c
@@ -0,0 +1,15 @@
+/**
+ * Author: Jack Robbins
+ * 03/29/2024
+ * Homework 05, only problem(Radix sort of signed floats)
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int main(void){
+	//Predeclare the functions that will be used
+	void radix_sort_unsigned(unsigned int* arr, int size);
+	void to_signed_order(unsigned int* arr, int size);
+}
---
+++ src/run.sh
@@ -0,0 +1,30 @@
+#Author: Jack Robbins
+#A simple runner script that allows users to test the floating point radix sort
+
+#!/bin/bash
+count=$1
+
+#recompile
+gcc -Wall -Wextra radix_sort_floats.c -o radix_sort_floats
+
+#Remove all old files
+rm floats
+rm radix_sorted 
+rm standard_sorted
+
+#Randomly generate floating point input
+echo "================== input ======================"
+echo ${count} | tee floats 
+for (( i=0; i<${count}; i++ ));
+do
+    printf "%d.%d\n" $((($RANDOM-$RANDOM)%1000)) $RANDOM
+done | tee -a floats
+
+#Display the radix sort execution result
+echo "============= execution result ================"
+cat floats | radix_sort_floats | xargs printf "%.2f\n" | tee radix_sorted
+tail -n +2 floats | sort -n | xargs printf "%.2f\n" > standard_sorted
+
+#Display any differences between the generic sort and radix sort
+echo "====== differences from correct result ======="
+diff radix_sorted  standard_sorted
